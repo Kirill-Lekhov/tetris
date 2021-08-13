28481d6652c9577708430f6968b18f0caae8de4d
Kirill Lekhov <kirill.lekhov@gmail.com>
2021-08-13 13:01:20 +0300

    Added Game GUI stage

--- Game_Stages/game.py
+++ Game_Stages/game.py
@@ -1,61 +1,14 @@
-from GUI.label import Label
-from GUI.time import Time
-from GUI.text_button import TextButton
-
-from pygame.sprite import Group
 from pygame.mixer import find_channel
 from pygame import QUIT, K_UP, KEYUP, KEYDOWN, Color, K_DOWN, K_ESCAPE, K_p, K_PAUSE
 
 from Tools.game_file_functions import save_game, push_records
 from Tools.os_tools import terminate
 from Game_Parts.game import Game
-from GUI.GUI import GUI
 
 
 def game_stage(game_pygame, clock, music, screen, game_settings, fon_picture):
     old_pixels, score, time, player_nickname = game_settings
-
-    #
-    buttons = Group()
-    #
-
-    game_score = score
-    gui = GUI()
-
     game = Game(screen, old_pixels)
-    text_score_head = Label((400, 75, 150, 65), "Score:", "White", -1)
-    text_score = Label((400, 135, 150, 50), str(game_score), "White", -1)
-    text_next_shape = Label((400, 200, 150, 50), "Next Shape", "White", -1)
-    text_pause_shape = Label((80, 200, 200, 200), "PAUSE", "blue", -1)
-    text_time = Label((400, 365, 150, 50), "Time:", "white", -1)
-
-    text_help_1 = Label((390, 450, 25, 25), "P:Пауза", "white", -1)
-    text_help_2 = Label((390, 475, 25, 25), "Esc:Выход", "white", -1)
-    text_help_3 = Label((390, 500, 25, 25), "Влево\вправо:Перемещение", "white", -1)
-    text_help_4 = Label((390, 525, 25, 25), "Вверх:Поворот", "white", -1)
-    text_help_5 = Label((390, 550, 25, 25), "Вниз:Ускорить падение", "white", -1)
-    dilog = Label((90, 275, 45, 45), "Вы уверены что хотите выйти?", "white", -1)
-    choice_buttons = [TextButton((100, 340), buttons, "Да", 'white'), TextButton((325, 340), buttons, "Нет", 'white')]
-
-    time = Time((450, 400, 150, 50), 'white', -1, time)
-
-    gui.add_element(text_score_head)
-    gui.add_element(text_score)
-    gui.add_element(text_next_shape)
-    gui.add_element(game.get_next_shape())
-    gui.add_element(text_time)
-    gui.add_element(text_help_1)
-    gui.add_element(text_help_2)
-    gui.add_element(text_help_3)
-    gui.add_element(text_help_4)
-    gui.add_element(text_help_5)
-    gui.add_element(time)
-
-    rungame = True
-
-    N = 1
-    pause = False
-    dialog_window = False
 
     while rungame:
         if not game.get_info():
